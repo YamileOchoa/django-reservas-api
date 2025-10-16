7a95643f50d0f829d05f698308bec443fa42b420
Yamile Ochoa <yamileochoa137@gmail.com>
2025-10-15 22:34:30 -0600

    Creación de ViewSets para Cliente y Reserva con filtros de búsqueda

--- src/reservations/views.py
+++ src/reservations/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Cliente, Reserva
+from .serializers import ClienteSerializer, ReservaSerializer
 
-# Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+    # Búsqueda por nombre o documento
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'documento']
+
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
+    # Búsqueda por número de habitación o nombre del cliente
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['numero_habitacion', 'cliente__nombre']
